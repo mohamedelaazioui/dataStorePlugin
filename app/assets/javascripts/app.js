db86b3339d121b6c28d238ad2e1510d8226cdb16
MohamedEA <mohamed.elaazioui@gmail.com>
2015-08-20 22:56:32 +0200

    request modelling added

--- app/assets/javascripts/app.js
+++ app/assets/javascripts/app.js
@@ -9,20 +9,28 @@ angular.module('dataStore',['templates','ngRoute','ngResource'])
                 templateUrl: 'templates/landing.html',
                 controller: 'welcomeCtrl'
             }).
+            when('/requests-list',{
+                templateUrl: 'templates/requests-list.html',
+                controller: 'requestsListCtrl'
+            }).
+            when('/create-request',{
+                templateUrl: 'templates/create-request.html',
+                controller: 'requestCtrl'
+            }).
             when('/branches-list',{
                 templateUrl: 'templates/branches-list.html',
                 controller: 'branchesListCtrl'
             }).
             when('/create-branch',{
-                templateUrl: 'templates/create-branch',
+                templateUrl: 'templates/create-branch.html',
                 controller: 'branchCtrl'
             }).
             when('/clients-list',{
-                templateUrl: 'templates/clients-list',
+                templateUrl: 'templates/clients-list.html',
                 controller: 'clientsListCtrl'
             }).
             when('/create-client',{
-                templateUrl: 'templates/create-client',
+                templateUrl: 'templates/create-client.html',
                 controller: 'clientCtrl'
             }).
             otherwise({
@@ -53,6 +61,28 @@ angular.module('dataStore',['templates','ngRoute','ngResource'])
             }
         });
     }])
+    .controller('requestsListCtrl',['$scope', function($scope){
+        $scope.requests = [
+            {'ID': 'TDC-281',
+             'Description': 'One Fortis user with One Current Account',
+                'Date': '18/08/2015',
+                'Project': '9066'
+            },
+            {'ID': 'TDC-282',
+             'Description': 'One Fortis user with One Current account and One Domestic Beneficiary',
+             'Date': '19/08/2015',
+            'Project': '8022'
+            },
+            {
+             'ID': 'TDC-230',
+             'Description': 'One Fortis user with One Current account and the account must have 1000 euro positive saldo on it',
+              'Date': '20/08/2015',
+              'Project': '8050'
+            }
+
+        ];
+        $scope.orderReq = '-Date';
+    }])
     .controller('branchCtrl',['$scope','Branch',function($scope,Branch){
         $scope.branch = new Branch();
 
@@ -76,5 +106,34 @@ angular.module('dataStore',['templates','ngRoute','ngResource'])
         };
     }])
     .controller('welcomeCtrl',['$scope',function($scope){
+        $scope.format = 'M/d/yy h:mm:ss a';
         $scope.date = new Date();
+    }])
+    .directive('myCurrentTime',['$interval','dateFilter',function($interval, dateFilter){
+        function link(scope, element, attrs) {
+            var format,
+                timeoutId;
+
+            function updateTime() {
+                element.text(dateFilter(new Date(), format));
+            }
+
+            scope.$watch(attrs.myCurrentTime, function(value) {
+                format = value;
+                updateTime();
+            });
+
+            element.on('$destroy', function() {
+                $interval.cancel(timeoutId);
+            });
+
+            // start the UI update process; save the timeoutId for canceling
+            timeoutId = $interval(function() {
+                updateTime(); // update DOM
+            }, 1000);
+        }
+
+        return {
+            link: link
+        };
     }]);
